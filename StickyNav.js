0d2dc87adb9f9e06e668593c10b91ccbe72d0555
Marc-André Lavigne <marcandre.lavigne@me.com>
2019-04-15 15:28:28 -0400

    Update StickyNav.js

--- StickyNav.js
+++ StickyNav.js
@@ -12,13 +12,13 @@ class StickyNav extends Component {
 
 	handleScroll(event) {
 	    let scrollpos = window.scrollY
-		const header = document.querySelector('nav')
-		const header_height = header.offsetHeight
-		const add_class_on_scroll = () => header.classList.add('is-sticked')
-		const remove_class_on_scroll = () => header.classList.remove('is-sticked')
+		const nav = document.querySelector('nav')
+		const nav_height = nav.offsetHeight
+		const add_class_on_scroll = () => nav.classList.add('is-sticked')
+		const remove_class_on_scroll = () => nav.classList.remove('is-sticked')
 
 		scrollpos = window.scrollY;
-		if (scrollpos >= header_height) {
+		if (scrollpos >= nav_height) {
 		  add_class_on_scroll()
 		} else {
 		  remove_class_on_scroll()
